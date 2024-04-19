6efde293a4ceb3ab8bfec1f3b20ec8aa33ad2d5d
hlibmurphy <minexgleb@gmail.com>
2024-04-19 19:56:42 +0300

    Add lacking files

---
+++ src/main/java/com/github/edocapi/EDocApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.edocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDocApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDocApiApplication.class, args);
+    }
+
+}
